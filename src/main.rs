4516cd8b02700b387c9edca05d626e4c0248074b
Joonas Muhonen <joonas.muhonen@protonmail.com>
2022-04-11 20:31:35 +0300

    Multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::{HashMap, HashSet};
-use std::{env, io};
+use std::{env, io, thread};
 use std::fs::File;
-use std::io::{BufRead, BufReader, Seek, Write};
+use std::io::{BufRead, BufReader, Write};
+use std::sync::{Arc, Mutex};
 use config::Config;
+use crossbeam_channel::{bounded, Receiver};
 use log::{error, info, warn};
 use nom::IResult;
 use nom::bytes::complete::{take_until};
@@ -11,7 +13,7 @@ use nom::character::complete;
 use nom::sequence::{delimited, terminated};
 use serde::Deserialize;
 
-#[derive(Eq, PartialEq, Hash, Deserialize)]
+#[derive(Eq, PartialEq, Hash, Deserialize, Clone)]
 struct TileLocation {
     x: u16,
     y: u16,
@@ -28,7 +30,7 @@ impl TileLocation {
     }
 }
 
-#[derive(Eq, PartialEq, Hash, Deserialize)]
+#[derive(Eq, PartialEq, Hash, Deserialize, Clone)]
 struct TileRegion {
     start: TileLocation,
     end: TileLocation,
@@ -119,6 +121,7 @@ struct Settings {
     csv_location: String,
     end_time: String,
     search_areas: Vec<TileRegion>,
+    no_edits_outside: bool,
 }
 
 fn main() {
@@ -141,79 +144,91 @@ fn main() {
         .expect("Failed to set default csv location")
         .set_default("end_time", "2022-04-04 21:32:37.541 UTC")
         .expect("Failed to set default end time")
+        .set_default("no_edits_outside", true)
+        .expect("Failed to set default no_edits_outside")
         .add_source(config::File::with_name(config_path).required(has_config_path))
         .build()
         .expect("Configuration file contains errors");
     let settings: Settings = config.try_deserialize()
         .expect("Failed to parse configurations");
 
+    //HashMap of users who have edits in selected areas
+    let users = Arc::new(Mutex::new(
+        HashMap::<String, HashSet<TileRegion>>::new()));
+
     //Get list of potential users in selected areas
-    let potential_users = find_users_in_area(&settings.search_areas, &settings.csv_location);
-    if potential_users.is_empty() {
-        println!("Did not find any users.");
-        return;
+    let locations = Arc::new(settings.search_areas);
+    find_users_in_area(locations.clone(), &settings.csv_location, users.clone());
+    //If enabled remove users who have edits outside selected areas
+    if settings.no_edits_outside {
+        remove_users_who_edited_outside(locations.clone(), &settings.csv_location, users.clone());
     }
 
-    println!("Found users:");
-    for (index, user) in potential_users.iter().enumerate() {
-        println!("{}: {}", index, user);
-    }
+    //Remove uses who did not have edits in all selected areas
+    match users.lock() {
+        Ok(mut g) => {
+            //Remove elements which were not found in all selected areas
+            g.retain(|_, regions| {
+                regions.len() == locations.len()
+            });
+            let potential_users: Vec<String> = g.clone().into_keys().collect();
+            if potential_users.is_empty() {
+                println!("Did not find any users.");
+                return;
+            }
 
-    print!("Select user by giving index: ");
-    if let Err(e) = io::stdout().flush() {
-        error!("Failed to flush stdout: {}", e);
-    }
-    let input;
-    loop {
-        let mut user_input = String::new();
-        if let Err(e) = io::stdin().read_line(&mut user_input) {
-            error!("Failed to read input: {}", e);
-            continue;
-        }
-        //Remove trailing newline
-        user_input.pop();
-        let user_index: usize = match user_input.parse() {
-            Ok(v) => {
-                if v >= potential_users.len() {
-                    eprintln!("Index out of bounds");
-                    continue;
+            println!("Found users:");
+            for (index, user) in potential_users.iter().enumerate() {
+                println!("{}: {}", index, user);
+            }
+
+            let input;
+            if potential_users.len() > 1 {
+                print!("Select user by giving index: ");
+                if let Err(e) = io::stdout().flush() {
+                    error!("Failed to flush stdout: {}", e);
                 }
-                v
-            }
-            Err(_) => {
-                eprintln!("Give zero based index of user you want to select");
-                continue;
-            }
-        };
-        input = user_index;
-        break;
-    }
+                loop {
+                    let mut user_input = String::new();
+                    if let Err(e) = io::stdin().read_line(&mut user_input) {
+                        error!("Failed to read input: {}", e);
+                        continue;
+                    }
+                    //Remove trailing newline
+                    user_input.pop();
+                    let user_index: usize = match user_input.parse() {
+                        Ok(v) => {
+                            if v >= potential_users.len() {
+                                eprintln!("Index out of bounds");
+                                continue;
+                            }
+                            v
+                        }
+                        Err(_) => {
+                            eprintln!("Give zero based index of user you want to select");
+                            continue;
+                        }
+                    };
+                    input = user_index;
+                    break;
+                }
 
-    let user_hash = &potential_users[input];
-    find_remaining_tiles(user_hash, &settings.csv_location, &settings.end_time);
+            } else {
+                input = 0;
+            }
+
+            let user_hash = &potential_users[input];
+            find_remaining_tiles(user_hash, &settings.csv_location, &settings.end_time);
+        }
+        Err(e) => {
+            eprintln!("Mutex lock failed: {}", e);
+        }
+    };
 }
 
-/**
- * Find users who only placed tiles at locations specified in locations
- */
-fn find_users_in_area(locations: &Vec<TileRegion>, file_name: &str) -> Vec<String> {
-    let mut file = File::open(file_name).expect("Failed to open tile data");
-    let reader = BufReader::new(&file);
-
-    //Iterate over rows to find ALL users who placed tiles inside locations
-    let mut users = HashMap::<&TileRegion, HashSet<String>>::new();
-    let mut line_reader = reader.lines();
-    if line_reader.next().is_none() {
-        panic!("Could not skip CSV header");
-    };
-    for line_result in line_reader {
-        let line = match line_result {
-            Ok(l) => { l }
-            Err(e) => {
-                warn!("Failed to obtain line from tile data: {}", e);
-                continue;
-            }
-        };
+fn process_line(users: Arc<Mutex<HashMap<String, HashSet<TileRegion>>>>, receiver: Receiver<String>, locations: Arc<Vec<TileRegion>>) {
+    for line in receiver {
+        //Convert line to struct
         let row_result = match CanvasLine::parse(&line) {
             Ok((_, v)) => { v }
             Err(_) => {
@@ -221,44 +236,96 @@ fn find_users_in_area(locations: &Vec<TileRegion>, file_name: &str) -> Vec<Strin
                 continue;
             }
         };
-
-        for location in locations {
+        //Check if coordinates in selected areas
+        for location in &*locations {
             match &row_result.coordinate {
                 LineCoordinate::Tile(t) => {
-                    if location.contains(t) {
-                        let user_list = users.entry(location).or_insert_with(|| { HashSet::<String>::new() });
-                        user_list.insert(row_result.user_id.clone());
-                        break;
+                    if !location.contains(t) {
+                        continue;
+                    }
+                    //Coordinates match region
+                    match users.lock() {
+                        Ok(mut g) => {
+                            let region_set = g.entry(row_result.user_id.clone())
+                                .or_insert_with(|| { HashSet::<TileRegion>::new() });
+                            region_set.insert(location.clone());
+                        }
+                        Err(e) => {
+                            eprintln!("Mutex lock failed: {}", e);
+                        }
                     }
                 }
                 LineCoordinate::Region(r) => {
-                    if location.intersects(r) {
-                        let user_list = users.entry(location).or_insert_with(|| { HashSet::<String>::new() });
-                        user_list.insert(row_result.user_id.clone());
-                        break;
+                    if !location.intersects(r) {
+                        continue;
+                    }
+                    match users.lock() {
+                        Ok(mut g) => {
+                            let region_set = g.entry(row_result.user_id.clone())
+                                .or_insert_with(|| { HashSet::<TileRegion>::new() });
+                            region_set.insert(location.clone());
+                        }
+                        Err(e) => {
+                            eprintln!("Mutex lock failed: {}", e);
+                        }
                     }
                 }
             }
         }
     }
+}
 
-    //New reader to read from start
-    if let Err(e) = file.rewind() {
-        panic!("Could not rewind file: {}", e);
-    }
+/**
+ * Find users who only placed tiles at locations specified in locations
+ */
+fn find_users_in_area(locations: Arc<Vec<TileRegion>>, file_name: &str, users: Arc<Mutex<HashMap<String, HashSet<TileRegion>>>>) {
+    let (sender, receiver) = bounded(2048);
+
+    let file = File::open(file_name)
+        .expect("Failed to open tile data");
     let reader = BufReader::new(&file);
+
+    //Double thread count to optimize scheduler behaviour
+    let thread_count = num_cpus::get() * 2;
+    //Get HashSet of all users who have placed tiles in selected areas
+    let mut thread_handles = Vec::with_capacity(thread_count);
+    for _ in 0..thread_count {
+        let receiver_clone = receiver.clone();
+        let user_clone = users.clone();
+        let location_clone = locations.clone();
+        thread_handles.push(thread::spawn(move || {
+            process_line(user_clone, receiver_clone, location_clone);
+        }));
+    }
+
+    //Iterate over rows to find ALL users who placed tiles inside locations
     let mut line_reader = reader.lines();
     if line_reader.next().is_none() {
         panic!("Could not skip CSV header");
     };
+
     for line_result in line_reader {
-        let line = match line_result {
-            Ok(l) => { l }
+        match line_result {
+            Ok(l) => {
+                sender.send(l)
+                    .expect("Can not send value, channel closed unexpectedly");
+            }
             Err(e) => {
                 warn!("Failed to obtain line from tile data: {}", e);
-                continue;
             }
         };
+    }
+    //Drop sender so threads shut down
+    drop(sender);
+
+    //Wait for threads to finish
+    for i in thread_handles {
+        i.join().unwrap();
+    }
+}
+
+fn process_line_removal(users: Arc<Mutex<HashMap<String, HashSet<TileRegion>>>>, receiver: Receiver<String>, locations: Arc<Vec<TileRegion>>) {
+    for line in receiver {
         let row_result = match CanvasLine::parse(&line) {
             Ok((_, v)) => { v }
             Err(_) => {
@@ -269,7 +336,7 @@ fn find_users_in_area(locations: &Vec<TileRegion>, file_name: &str) -> Vec<Strin
 
         //Remove users who have edits outside locations
         let mut is_outside = true;
-        for location in locations {
+        for location in &*locations {
             match &row_result.coordinate {
                 LineCoordinate::Tile(t) => {
                     if location.contains(t) {
@@ -287,33 +354,62 @@ fn find_users_in_area(locations: &Vec<TileRegion>, file_name: &str) -> Vec<Strin
         }
         //Edit is not in any selected area
         if is_outside {
-            for location in locations {
-                if let Some(v) = users.get_mut(location) {
-                    v.remove(&row_result.user_id);
+            match users.lock() {
+                Ok(mut g) => {
+                    g.remove(&row_result.user_id);
+                }
+                Err(e) => {
+                    eprintln!("Mutex lock failed: {}", e);
                 }
             }
         }
     }
+}
 
-    //Calculate intersections of all users
-    let mut iterator = users.values();
-    let mut first = match iterator.next() {
-        None => { panic!("No regions selected!") }
-        Some(v) => { v.clone() }
+fn remove_users_who_edited_outside(locations: Arc<Vec<TileRegion>>, file_name: &str, users: Arc<Mutex<HashMap<String, HashSet<TileRegion>>>>) {
+    let (sender, receiver) = bounded(2048);
+
+    let file = File::open(file_name)
+        .expect("Failed to open tile data");
+    let reader = BufReader::new(&file);
+
+    //Double thread count to optimize scheduler behaviour
+    let thread_count = num_cpus::get() * 2;
+    //Get HashSet of all users who have placed tiles in selected areas
+    let mut thread_handles = Vec::with_capacity(thread_count);
+    for _ in 0..thread_count {
+        let receiver_clone = receiver.clone();
+        let user_clone = users.clone();
+        let location_clone = locations.clone();
+        thread_handles.push(thread::spawn(move || {
+            process_line_removal(user_clone, receiver_clone, location_clone);
+        }));
+    }
+
+    //Iterate over rows to find ALL users who placed tiles inside locations
+    let mut line_reader = reader.lines();
+    if line_reader.next().is_none() {
+        panic!("Could not skip CSV header");
     };
-    for i in iterator {
-        let mut new_set = HashSet::<String>::new();
-        for x in first.intersection(i) {
-            new_set.insert(x.clone());
-        }
-        first = new_set;
-    }
 
-    let mut results = Vec::<String>::new();
-    for i in first {
-        results.push(i);
+    for line_result in line_reader {
+        match line_result {
+            Ok(l) => {
+                sender.send(l)
+                    .expect("Can not send value, channel closed unexpectedly");
+            }
+            Err(e) => {
+                warn!("Failed to obtain line from tile data: {}", e);
+            }
+        };
     }
-    results
+    //Drop sender so threads shut down
+    drop(sender);
+
+    //Wait for threads to finish
+    for i in thread_handles {
+        i.join().unwrap();
+    }
 }
 
 /**
